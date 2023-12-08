3aa9f3a45b46fbff522ca068082a594891373d48
youve <25477622+youve@users.noreply.github.com>
2023-12-08 12:40:19 +0100

    Day 2 part 1

--- 02-cube-conundrum/solve.py
+++ 02-cube-conundrum/solve.py
@@ -15,25 +15,47 @@ Usage:
 '''
 
 def parse_input(lines):
-    data = []
+    data = {}
     try:
         for line in lines:
-            data.append(line.strip())
+            game, sets = line.strip().split(':')
+            game = int(game.split()[1])
+            data[game] = sets.split(';')
     except FileNotFoundError:
         # Using fileinput is so handy but I also want to specify part 1 or part 2 from the command line
         pass
     return data
 
 def solve_input_1(data):
-    answer = data
+    answer = 0
+    for game, sets in data.items():
+        if is_valid(sets):
+            answer += game
     return answer
+
+def is_valid(sets):
+    max_red = 12
+    max_green = 13
+    max_blue = 14
+    for set in sets:
+        colors = set.split(',')
+        for color in colors:
+            number, which_color = color.split()
+            if which_color == 'red' and int(number) > 12:
+                return False
+            if which_color == 'green' and int(number) > 13:
+                return False
+            if which_color == 'blue' and int(number) > 14:
+                return False
+    return True
+
 
 def solve_input_2(data):
     answer = data
     return answer
 
 try:
-    assert 'input' == sys.argv[-2]
+    assert 'input' in sys.argv[-2]
     assert sys.argv[-1] in ['1', '2']
     if '1' == sys.argv[-1]:
         data = parse_input(fileinput.input())
